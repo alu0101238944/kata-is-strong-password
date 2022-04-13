5a0a40b27ec9251397f9386645fd7a066f02bdfa
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-04-13 20:06:28 +0100

    Red: Password must have at least one digit

--- test_is_strong_password.py
+++ test_is_strong_password.py
@@ -18,6 +18,9 @@ class IsStrongPasswordTest(unittest.TestCase):
   def test_password_must_have_at_least_one_lowercase_letter(self):
     self.assertEqual(self.is_strong_password.apply('ABCD_1'), False)
 
+  def test_password_must_have_at_least_one_digit(self):
+    self.assertEqual(self.is_strong_password.apply('ABabc_'), False)
+
 
 if __name__ == '__main__':
   unittest.main()
