89870ea61c95a483e8881ff1a97f4792aa3ca443
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-04-13 20:03:09 +0100

    Red: Password must have at least one lowercase letter

--- test_is_strong_password.py
+++ test_is_strong_password.py
@@ -15,6 +15,9 @@ class IsStrongPasswordTest(unittest.TestCase):
   def test_password_must_have_at_least_one_uppercase_letter(self):
     self.assertEqual(self.is_strong_password.apply('abcd_1'), False)
 
+  def test_password_must_have_at_least_one_lowercase_letter(self):
+    self.assertEqual(self.is_strong_password.apply('ABCD_1'), False)
+
 
 if __name__ == '__main__':
   unittest.main()
