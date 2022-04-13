de4b8ea6c9c50774939a14f65348a0235b14432e
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-04-13 19:48:12 +0100

    Red: Password must have at least six characters

--- test_is_strong_password.py
+++ test_is_strong_password.py
@@ -6,5 +6,13 @@ class IsStrongPasswordTest(unittest.TestCase):
   def test_strong_password_return_true(self):
     self.assertEqual(is_strong_password('ABab_1'), True)
 
+  def test_password_must_have_at_least_six_characters(self):
+    self.assertEqual(is_strong_password(''), False)
+    self.assertEqual(is_strong_password('A'), False)
+    self.assertEqual(is_strong_password('Aa'), False)
+    self.assertEqual(is_strong_password('Aa1'), False)
+    self.assertEqual(is_strong_password('Aa1_'), False)
+    self.assertEqual(is_strong_password('Aa1_b'), False)
+
 if __name__ == '__main__':
   unittest.main()
