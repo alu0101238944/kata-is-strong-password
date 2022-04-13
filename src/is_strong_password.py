4cafed996868584ce80e0dee983b51f65beeb76c
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-04-13 20:04:48 +0100

    Green: Password must have at least one lowercase letter

--- src/is_strong_password.py
+++ src/is_strong_password.py
@@ -3,5 +3,12 @@ class IsStrongPassword():
   def __thereIsUpperCaseLetter(self, password):
     return any(map(lambda char: char.isupper(), password))
 
+  def __thereIsLowerCaseLetter(self, password):
+    return any(map(lambda char: char.islower(), password))
+
   def apply(self, password: str):
-    return len(password) >= 6 and self.__thereIsUpperCaseLetter(password)
+    return (
+      len(password) >= 6 and
+      self.__thereIsUpperCaseLetter(password) and
+      self.__thereIsLowerCaseLetter(password)
+    )
