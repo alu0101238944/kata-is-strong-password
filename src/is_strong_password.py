bbf6a7654377663e6fec0ab4e5d4f0b7ad3fabee
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-04-13 20:09:01 +0100

    Green: Password must have at least one digit

--- src/is_strong_password.py
+++ src/is_strong_password.py
@@ -6,9 +6,13 @@ class IsStrongPassword():
   def __thereIsLowerCaseLetter(self, password):
     return any(map(lambda char: char.islower(), password))
 
+  def __thereIsDigit(self, password):
+    return any(map(lambda char: char.isdigit(), password))
+
   def apply(self, password: str):
     return (
       len(password) >= 6 and
       self.__thereIsUpperCaseLetter(password) and
-      self.__thereIsLowerCaseLetter(password)
+      self.__thereIsLowerCaseLetter(password) and 
+      self.__thereIsDigit(password)
     )
